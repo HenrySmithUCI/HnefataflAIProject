8db9db8a72470b0e42ca40862ff4ade8dad859b0
HenrySmithUCI <hgsmith@uci.edu>
2019-03-19 15:22:08 -0700

    Added Undo

--- BoardGame.py
+++ BoardGame.py
@@ -11,6 +11,13 @@ class Piece:
         self.IsKing = isKing
         self.Alive = True
 
+class Move:
+    def __init__(self, fromX, fromY, toX, toY, piecesKilled = []):
+        self.fromX = fromX
+        self.fromY = fromY
+        self.toX = toX
+        self.toY = toY
+        self.piecesKilled = piecesKilled
 
 class GameBoard:
     def __init__(self, copy = None):
@@ -23,6 +30,7 @@ class GameBoard:
         
         if(copy == None):
             self.CurrentTurn = BLACK
+            self.History = []
             self.Pieces = [
                 Piece(self, BLACK, 3, 0),
                 Piece(self, BLACK, 3, 1),
@@ -41,6 +49,7 @@ class GameBoard:
             ]
         else:
             self.CurrentTurn = copy.CurrentTurn
+            self.History = []
             self.Pieces = []
             for p in copy.Pieces:
                 self.Pieces.append(Piece(self, p.Team, p.X, p.Y, p.IsKing, p.Alive))
@@ -53,17 +62,23 @@ class GameBoard:
         self.WhiteKing = self.Pieces[8]
         self.WhitePieces = self.Pieces[-5:]
 
-    def copy(self):
-        ret = GameBoard()
+    def Undo(self):
+        if(len(self.History) == 0):
+            return
+        move = self.History.pop()
+        movedP = self.Board[move.toX][move.toY]
+        movedP.posX = move.fromX
+        movedP.posY = move.fromY
+        self.Board[move.toX][move.toY] = None
+        self.Board[move.fromX][move.fromY] = movedP
+        for p in move.piecesKilled:
+            p.Alive = True
+            self.Board[p.PosX][p.PosY] = p
+        if (self.CurrentTurn == WHITE):
+            self.CurrentTurn = BLACK
+        elif (self.CurrentTurn == BLACK):
+            self.CurrentTurn = WHITE
         
-
-        ret.Board = []
-        for x in range(7):
-            ret.Board.append([])
-            for y in range(7):
-                board[x].append(None)
-        
-        return ret
 
     def __str__(self):
         s = "/ 0 1 2 3 4 5 6\n"
@@ -90,27 +105,29 @@ class GameBoard:
             s += "\n"
         return s
 
-    def IsValidMove(self, fromX, fromY, toX, toY):
+    def IsValidMove(self, fromX, fromY, toX, toY, getReason = False):
+        if(getReason == False):
+            return IsValidMove(fromX, fromY, toX, toY, True)[0]
 
         # Positions out of Bounds
         if (fromX < 0 or fromX > 6 or fromY < 0 or fromY > 6 or
                 toX < 0 or toX > 6 or toY < 0 or toY > 6):
-            return False
+            return (False,"Positions out of Bounds")
 
         fromP = self.Board[fromX][fromY]
         toP = self.Board[toX][toY]
 
         # Either from is empty or to is not
         if fromP is None or toP is not None:
-            return False
+            return (False,"Either from is empty or to is not")
 
         # Cannot move to the center
         if toX == 3 and toY == 3:
-            return False
+            return (False,"Cannot move to the center")
 
         # Wrong Piece Moving
         if fromP.Team != self.CurrentTurn:
-            return False
+            return (False,"Wrong Piece Moving")
 
         # Unless from is the king, cannot move to corners
         if (not fromP.IsKing):
@@ -118,84 +135,39 @@ class GameBoard:
                     (toX == 0 and toY == 6) or
                     (toX == 6 and toY == 0) or
                     (toX == 6 and toY == 6)):
-                return False
+                return (False,"Unless from is the king, cannot move to corners")
 
         if (fromX == toX):
             # Both Equal
             if (fromY == toY):
-                return False
+                return (False,"Both Equal")
 
             for y in range(min(fromY, toY), max(fromY, toY)):
                 if (y == fromY):
                     continue
                 # There is a piece between from and to
                 if (self.Board[fromX][y] != None):
-                    return False
+                    return (False,"There is a piece between from and to")
         else:
             # Both Unequal
             if (fromY != toY):
-                return False
+                return (False,"Both Unequal")
 
             for x in range(min(fromX, toX), max(fromX, toX)):
                 if (x == fromX):
                     continue
                 # There is a piece between from and to
                 if (self.Board[x][fromY] != None):
-                    return False
+                    return (False,"There is a piece between from and to")
 
-        return True
-
-    def GetReasonInvalid(self, fromX, fromY, toX, toY):
-
-        if (fromX < 0 or fromX > 6 or fromY < 0 or fromY > 6 or
-                toX < 0 or toX > 6 or toY < 0 or toY > 6):
-            return "Positions out of Bounds"
-
-        fromP = self.Board[fromX][fromY]
-        toP = self.Board[toX][toY]
-
-        if (fromP == None or toP != None):
-            return "Either from is empty or to is not"
-
-        if (toX == 3 and toY == 3):
-            return "Cannot move to the center"
-
-        if (fromP.Team != self.CurrentTurn):
-            return "Wrong Piece Moving"
-
-        if (not fromP.IsKing):
-            if ((toX == 0 and toY == 0) or
-                    (toX == 0 and toY == 6) or
-                    (toX == 6 and toY == 0) or
-                    (toX == 6 and toY == 6)):
-                return "Unless from is the king, cannot move to corners"
-
-        if (fromX == toX):
-            if (fromY == toY):
-                return "Both Equal"
-
-            for y in range(min(fromY, toY), max(fromY, toY)):
-                if (y == fromY):
-                    continue
-                if (self.Board[fromX][y] != None):
-                    return "There is a piece between from and to"
-        else:
-            if (fromY != toY):
-                return "Both Unequal"
-
-            for x in range(min(fromX, toX), max(fromX, toX)):
-                if (x == fromX):
-                    continue
-                if (self.Board[x][fromY] != None):
-                    return "There is a piece between from and to"
-
-        return "Move is Valid"
+        return (True,"Move is Valid")
 
     def MakeMove(self, fromX, fromY, toX, toY, check=True):
         if (check):
             if (not self.IsValidMove(fromX, fromY, toX, toY)):
                 return False
 
+        move = Move(fromX, fromY, toX, toY)
         fromP = self.Board[fromX][fromY]
         self.Board[fromX][fromY] = None
         self.Board[toX][toY] = fromP
@@ -233,6 +205,7 @@ class GameBoard:
 
             if ((not capP.Team == fromP.Team) and checkP.Team == fromP.Team):
                 capP.Alive = False
+                move.piecesKilled.append(self.Board[toCap[0]][toCap[1]])
                 if (capP.IsKing == False):
                     self.Board[toCap[0]][toCap[1]] = None
 
@@ -240,6 +213,8 @@ class GameBoard:
             self.CurrentTurn = BLACK
         elif (self.CurrentTurn == BLACK):
             self.CurrentTurn = WHITE
+
+        self.History.append(move)
 
         return True
 
@@ -277,6 +252,9 @@ if __name__ == "__main__":
                 i = input()
                 if (i == "QUIT"):
                     break
+                if (i == "UNDO"):
+                    break
+                
                 s = i.split(" ")
 
                 fromX = int(s[0])
@@ -295,8 +273,11 @@ if __name__ == "__main__":
 
         if (i == "QUIT"):
             break
-
-        g.MakeMove(fromX, fromY, toX, toY, False)
+        
+        if (i == "UNDO"):
+            g.Undo()
+        else:
+            g.MakeMove(fromX, fromY, toX, toY, False)
 
         winner = g.GetWinner()
         if (winner != None):
