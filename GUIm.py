309494ea4ceb4483c34e4c7c6ed3246aa094909c
Kasen Chaque <kchaque@uci.edu>
2019-03-21 18:18:29 -0700

    Final GUI
    Added more features to GUI, removed debug code

--- GUIm.py
+++ GUIm.py
@@ -4,6 +4,7 @@ import MinimaxTreeM as MinimaxTree
 
 BLACK = 0
 WHITE = 1
+BOTH = 2
 WIDTH = 700
 HEIGHT = 700
 
@@ -18,56 +19,75 @@ class GUI:
         self.frame.grid(padx = 10, pady = 10)
         self.root.resizable(False, False)
         self.player = tk.IntVar()
-        tk.Label(self.frame, text = "Play as: ").grid(column = 0, row = 0, columnspan = 2)
+        tk.Label(self.frame, text = "Play as: ").grid(column = 0, row = 0, columnspan = 3)
         tk.Radiobutton(self.frame, text = "Black", variable = self.player, value = BLACK).grid(column = 0, row = 1)
         tk.Radiobutton(self.frame, text = "White", variable = self.player, value = WHITE).grid(column = 1, row = 1)
-        tk.Button(self.frame, text = "Start", command = self.startGame).grid(column = 0, row = 2, columnspan = 2)
+        tk.Radiobutton(self.frame, text = "Human vs. Human", variable = self.player, value = BOTH).grid(column = 2, row = 1)
+        tk.Button(self.frame, text = "Start", command = self.startGame).grid(column = 0, row = 2, columnspan = 3)
 
     def startGame(self):
         self.frame.destroy()
         self.canvas = tk.Canvas(self.root, height = HEIGHT, width = WIDTH, bg = 'antique white')
         self.canvas.grid(column = 0, row = 0, columnspan = 3)
         self.bottomText = tk.StringVar()
+        self.leftText = tk.StringVar()
         tk.Label(self.root, textvariable = self.bottomText, font = (None, 16, 'bold')).grid(column = 1, row = 1)
+        tk.Label(self.root, textvariable = self.leftText, font = (None, 12, 'bold'), justify = tk.LEFT).grid(column = 0, row = 1, sticky = tk.W)
         self.backButton = tk.Button(self.root, text = "Undo", command = self.undoMove)
         self.backButton.grid(column = 2, row = 1, sticky = tk.E)
         self.colWidth = WIDTH / 7
         self.root.bind("<ButtonRelease-1>", self.onClick)
         self.root.bind("<ButtonRelease-3>", self.rightClick)
         self.board = BoardGame.GameBoard()
+        self.updateLText()
         self.pieceSelected = None
         self.gameOver = False
         self.drawBoard()
-        if self.player.get() != self.board.CurrentTurn:
+        if self.player.get() == (not self.board.CurrentTurn):
             self.bottomText.set("Thinking...")
             self.backButton.config(state = tk.DISABLED)
             self.root.after(20, self.aiMove)
         else:
             self.bottomText.set("Select piece to move")
 
+    def updateLText(self, currentBoard = True):
+        pred = round(float(MinimaxTree.Net.Predict(self.board)), 6)
+        favored = "White" if pred < 0 else "Black"
+        turns = int(round(1 / abs(pred)))
+        state = 'current board state: ' if currentBoard else 'tested move: '
+        self.leftText.set("NN Heuristic output for " + state + str(pred) + '\n' + \
+                          favored + " favored to win. Estimated: " + str(turns) + " turns.")
+
     def aiMove(self):
         moveVal, move, hist = MinimaxTree.alphabeta(self.board)
         self.board.MakeMove(move.fromX, move.fromY, move.toX, move.toY, True)
-        print('Made move of value:', moveVal)
-        print('Tree history: ', hist)
         self.drawBoard()
         self.backButton.config(state = tk.NORMAL)
         winner = self.board.GetWinner()
         if winner != None:
             self.bottomText.set(("Black" if winner == BLACK else "White") + " wins.")
             self.gameOver = True
+            self.leftText.set("")
         else:
-            print("\n".join([str(x) for x in MinimaxTree.alphabeta(self.board, depthLeft = 3)]))
             self.bottomText.set("Select piece to move")
+            self.updateLText()
 
     def undoMove(self):
-        if(len(self.board.History) >= 2):
+        n = 1 if self.player.get() == BOTH else 2
+        if(len(self.board.History) >= n):
+            self.backButton.config(state = tk.DISABLED)
+            self.root.after(20, self.undo())
+
+    def undo(self):
+        self.board.Undo()
+        if self.player.get() != BOTH:
             self.board.Undo()
-            self.board.Undo()
-            self.pieceSelected = None
-            self.gameOver = False
-            self.drawBoard()
-            self.bottomText.set("Select piece to move")
+        self.pieceSelected = None
+        self.gameOver = False
+        self.drawBoard()
+        self.bottomText.set("Select piece to move")
+        self.updateLText()
+        self.backButton.config(state = tk.ACTIVE)
     
     def drawBoard(self):
         self.canvas.delete("all")
@@ -80,7 +100,7 @@ class GUI:
             self.canvas.create_line(0, self.colWidth * i, WIDTH, self.colWidth * i)
             self.canvas.create_line(self.colWidth * i, 0, self.colWidth * i, HEIGHT)
         for piece in self.board.Pieces:
-            if piece.Alive:
+            if piece.Alive or piece.IsKing:
                 x = self.colWidth * piece.X
                 y = self.colWidth * piece.Y
                 self.canvas.create_oval(x, y, x + self.colWidth, y + self.colWidth, \
@@ -94,7 +114,7 @@ class GUI:
             self.canvas.create_rectangle(x, y, x + self.colWidth, y + self.colWidth, outline = 'red', width = 2)
 
     def onClick(self, event):
-        if event.widget == self.canvas and self.board.CurrentTurn == self.player.get():
+        if event.widget == self.canvas and (self.board.CurrentTurn == self.player.get() or self.player.get() == BOTH):
             self.handleClick(int(event.x // self.colWidth), int(event.y // self.colWidth))
 
     def rightClick(self, event):
@@ -102,20 +122,21 @@ class GUI:
         y = int(event.y // self.colWidth)
         if self.pieceSelected:
             if self.board.MakeMove(self.pieceSelected[0], self.pieceSelected[1], x, y, True):
-                print(" ".join([str(x) for x in MinimaxTree.alphabeta(self.board, depthLeft = 2)]))
+                self.updateLText(False)
                 self.board.Undo()
 
     def handleClick(self, x, y):
         if not self.gameOver:
             if self.pieceSelected == None:
                 piece = self.board.Board[x][y]
-                if piece != None and piece.Team == self.player.get():
+                if piece != None and self.board.CurrentTurn == piece.Team and (piece.Team == self.player.get() or self.player.get() == BOTH):
                     self.pieceSelected = [x, y]
                     self.bottomText.set("Select destination")
                     self.drawBoard()
             elif self.pieceSelected[0] == x and self.pieceSelected[1] == y:
                 self.pieceSelected = None
                 self.bottomText.set("Select piece to move")
+                self.updateLText()
                 self.drawBoard()
             else:
                 if self.board.MakeMove(self.pieceSelected[0], self.pieceSelected[1], x, y, True):
@@ -124,11 +145,16 @@ class GUI:
                     self.drawBoard()
                     if winner != None:
                         self.bottomText.set(("Black" if winner == BLACK else "White") + " wins.")
+                        self.leftText.set("")
                         self.gameOver = True
                     else:
-                        self.bottomText.set("Thinking...")
-                        self.backButton.config(state = tk.DISABLED)
-                        self.root.after(20, self.aiMove)
+                        self.updateLText()
+                        if(self.player.get() != BOTH):
+                            self.bottomText.set("Thinking...")
+                            self.backButton.config(state = tk.DISABLED)
+                            self.root.after(20, self.aiMove)
+                        else:
+                            self.bottomText.set("Select piece to move")
                         
 
 app = GUI()
